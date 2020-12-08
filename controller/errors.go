f1ac854807822427f43f353ad14d6842b848fad5
Donnie Adams <donnie.adams@rancher.com>
2020-12-08 15:54:52 -0700

    Add support for launch templates and spot instances
    Everytime a new cluster is created, the operator creates a new launch
    template that will be managed behind the scenes. The launch template is
    tagged as managed by Rancher and it should not be modified or deleted.
    This launch template is deleted when the cluster is deleted.
    
    If a cluster is imported, then the operator looks for a launch template
    with the predefined name. It is used as a managed launch template. When
    an imported cluster is deleted, this template is not deleted because the
    cluster is not removed from EKS in this case.
    
    For each new node group, the user can use their own launch template. If
    they do not provide a launch template, then the operator creates a new
    version of the managed template specifically for this new node group. If
    a node group is updated, then a new version of the managed launch
    template is created and the old version is deleted.
    
    Support for spot instances is also added. If a user specifies "true" for
    spot instances, then the user should also specify a number of instance
    types via spotInstanceTypes.

---
+++ controller/errors.go
@@ -0,0 +1,62 @@
+package controller
+
+import (
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/cloudformation"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/eks"
+)
+
+func alreadyExistsInCloudFormationError(err error) bool {
+	if aerr, ok := err.(awserr.Error); ok {
+		switch aerr.Code() {
+		case cloudformation.ErrCodeAlreadyExistsException:
+			return true
+		}
+	}
+
+	return false
+}
+
+func isClusterConflict(err error) bool {
+	if awsErr, ok := err.(awserr.Error); ok {
+		return awsErr.Code() == eks.ErrCodeResourceInUseException
+	}
+
+	return false
+}
+
+func doesNotExist(err error) bool {
+	// There is no better way of doing this because AWS API does not distinguish between a attempt to delete a stack
+	// (or key pair) that does not exist, and, for example, a malformed delete request, so we have to parse the error
+	// message
+	if err != nil {
+		return strings.Contains(err.Error(), "does not exist")
+	}
+
+	return false
+}
+
+func launchTemplateVersionDoesNotExist(errorCode string) bool {
+	return errorCode == ec2.LaunchTemplateErrorCodeLaunchTemplateVersionDoesNotExist ||
+		errorCode == ec2.LaunchTemplateErrorCodeLaunchTemplateIdDoesNotExist
+}
+
+func notFound(err error) bool {
+	if awsErr, ok := err.(awserr.Error); ok {
+		return awsErr.Code() == eks.ErrCodeResourceNotFoundException ||
+			strings.Contains(awsErr.Code(), "VersionNotFound")
+	}
+
+	return false
+}
+
+// NodeGroupIssueIsUpdatable checks to see the node group can be updated with the given issue code.
+func NodeGroupIssueIsUpdatable(code string) bool {
+	return code == eks.NodegroupIssueCodeAsgInstanceLaunchFailures ||
+		code == eks.NodegroupIssueCodeInstanceLimitExceeded ||
+		code == eks.NodegroupIssueCodeInsufficientFreeAddresses ||
+		code == eks.NodegroupIssueCodeClusterUnreachable
+}
