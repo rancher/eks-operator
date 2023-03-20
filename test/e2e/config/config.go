b5c090ea9e30387aa56ecdc76dfa5f0e41a9a9e2
Alexandr Demicev <alexandr.demicev@suse.com>
2023-03-20 13:05:36 +0100

    Add e2e test setup

---
+++ test/e2e/config/config.go
@@ -0,0 +1,155 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/drone/envsubst/v2"
+	"sigs.k8s.io/yaml"
+)
+
+type E2EConfig struct {
+	OperatorChart string `yaml:"operatorChart"`
+	CRDChart      string `yaml:"crdChart"`
+	ExternalIP    string `yaml:"externalIP"`
+	MagicDNS      string `yaml:"magicDNS"`
+	BridgeIP      string `yaml:"bridgeIP"`
+	ArtifactsDir  string `yaml:"artifactsDir"`
+
+	CertManagerVersion  string `yaml:"certManagerVersion"`
+	CertManagerChartURL string `yaml:"certManagerChartURL"`
+
+	RancherVersion  string `yaml:"rancherVersion"`
+	RancherChartURL string `yaml:"rancherChartURL"`
+
+	AWSAccessKey       string `yaml:"awsAccessKey"`
+	AWSSecretAccessKey string `yaml:"awsSecretAccessKey"`
+
+	AWSRegion string `yaml:"awsRegion"`
+}
+
+// ReadE2EConfig read config from yaml and substitute variables using envsubst.
+// All variables can be overridden by environmental variables.
+func ReadE2EConfig(configPath string) (*E2EConfig, error) { //nolint:gocyclo
+	config := &E2EConfig{}
+
+	configData, err := os.ReadFile(configPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config file: %w", err)
+	}
+
+	if configData == nil {
+		return nil, errors.New("config file can't be empty")
+	}
+
+	if err := yaml.Unmarshal(configData, config); err != nil {
+		return nil, fmt.Errorf("failed to unmarhal config file: %s", err)
+	}
+
+	if operatorChart := os.Getenv("OPERATOR_CHART"); operatorChart != "" {
+		config.OperatorChart = operatorChart
+	}
+
+	if config.OperatorChart == "" {
+		return nil, errors.New("no OPERATOR_CHART provided, an operator helm chart is required to run e2e tests")
+	}
+
+	if crdChart := os.Getenv("CRD_CHART"); crdChart != "" {
+		config.CRDChart = crdChart
+	}
+
+	if config.CRDChart == "" {
+		return nil, errors.New("no CRD_CHART provided, a crd helm chart is required to run e2e tests")
+	}
+
+	if externalIP := os.Getenv("EXTERNAL_IP"); externalIP != "" {
+		config.ExternalIP = externalIP
+	}
+
+	if config.ExternalIP == "" {
+		return nil, errors.New("no EXTERNAL_IP provided, a known (reachable) node external ip it is required to run e2e tests")
+	}
+
+	if magicDNS := os.Getenv("MAGIC_DNS"); magicDNS != "" {
+		config.MagicDNS = magicDNS
+	}
+
+	if bridgeIP := os.Getenv("BRIDGE_IP"); bridgeIP != "" {
+		config.BridgeIP = bridgeIP
+	}
+
+	if artifactsDir := os.Getenv("ARTIFACTS_DIR"); artifactsDir != "" {
+		config.ArtifactsDir = artifactsDir
+	}
+
+	if awsAccessKey := os.Getenv("AWS_ACCESS_KEY_ID"); awsAccessKey != "" {
+		config.AWSAccessKey = awsAccessKey
+	}
+
+	if awsSecretAccessKey := os.Getenv("AWS_SECRET_ACCESS_KEY"); awsSecretAccessKey != "" {
+		config.AWSSecretAccessKey = awsSecretAccessKey
+	}
+
+	if awsRegion := os.Getenv("AWS_REGION"); awsRegion != "" {
+		config.AWSRegion = awsRegion
+	}
+
+	if certManagerVersion := os.Getenv("CERT_MANAGER_VERSION"); certManagerVersion != "" {
+		config.CertManagerVersion = certManagerVersion
+	}
+
+	if certManagerURL := os.Getenv("CERT_MANAGER_CHART_URL"); certManagerURL != "" {
+		config.CertManagerChartURL = certManagerURL
+	}
+
+	if rancherVersion := os.Getenv("RANCHER_VERSION"); rancherVersion != "" {
+		config.RancherVersion = rancherVersion
+	}
+
+	if rancherURL := os.Getenv("RANCHER_CHART_URL"); rancherURL != "" {
+		config.RancherChartURL = rancherURL
+	}
+
+	if err := substituteVersions(config); err != nil {
+		return nil, err
+	}
+
+	return config, validateAWSCredentials(config)
+}
+
+func substituteVersions(config *E2EConfig) error {
+	certManagerURL, err := envsubst.Eval(config.CertManagerChartURL, func(s string) string {
+		return config.CertManagerVersion
+	})
+	if err != nil {
+		return fmt.Errorf("failed to substiture cert manager chart url: %w", err)
+	}
+	config.CertManagerChartURL = certManagerURL
+
+	rancherURL, err := envsubst.Eval(config.RancherChartURL, func(s string) string {
+		return config.RancherVersion
+	})
+	if err != nil {
+		return fmt.Errorf("failed to substiture rancher chart url: %w", err)
+	}
+	config.RancherChartURL = rancherURL
+
+	return nil
+}
+
+func validateAWSCredentials(config *E2EConfig) error {
+	if config.AWSAccessKey == "" {
+		return errors.New("no AWS_ACCESS_KEY_ID provided, an aws access key is required to run e2e tests")
+	}
+
+	if config.AWSSecretAccessKey == "" {
+		return errors.New("no AWS_SECRET_ACCESS_KEY provided, an aws secret access key is required to run e2e tests")
+	}
+
+	if config.AWSRegion == "" {
+		return errors.New("no AWS_REGION provided, an aws region is required to run e2e tests")
+	}
+
+	return nil
+}
---
+++ test/e2e/suite_test.go
@@ -0,0 +1,331 @@
+package e2e
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	kubectl "github.com/rancher-sandbox/ele-testhelpers/kubectl"
+	eksv1 "github.com/rancher/eks-operator/pkg/apis/eks.cattle.io/v1"
+	e2eConfig "github.com/rancher/eks-operator/test/e2e/config"
+	managementv3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+	runtimeconfig "sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/yaml"
+)
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(clientgoscheme.Scheme))
+	utilruntime.Must(managementv3.AddToScheme(clientgoscheme.Scheme))
+	utilruntime.Must(apiextensionsv1.AddToScheme(clientgoscheme.Scheme))
+	utilruntime.Must(eksv1.AddToScheme(clientgoscheme.Scheme))
+}
+
+const (
+	operatorName              = "eks-config-operator"
+	crdChartName              = "eks-config-operator-crd"
+	certManagerNamespace      = "cert-manager"
+	certManagerName           = "cert-manager"
+	certManagerCAInjectorName = "cert-manager-cainjector"
+	awsCredentialsSecretName  = "aws-credentials"
+	cattleSystemNamespace     = "cattle-system"
+	rancherName               = "rancher"
+	eksClusterConfigNamespace = "cattle-global-data"
+)
+
+// Test configuration
+var (
+	e2eCfg   *e2eConfig.E2EConfig
+	cl       runtimeclient.Client
+	ctx      = context.Background()
+	crdNames = []string{
+		"eksclusterconfigs.eks.cattle.io",
+	}
+
+	pollInterval = 10 * time.Second
+	waitLong     = 15 * time.Minute
+)
+
+// Cluster Templates
+var (
+	// //go:embed templates/* TODO: uncomment with first tests
+	// templates embed.FS
+
+	clusterTemplates         = map[string]*eksv1.EKSClusterConfig{}
+	basicClusterTemplateName = "basic-cluster"
+)
+
+func TestE2e(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "eks-operator e2e test Suite")
+}
+
+var _ = BeforeSuite(func() {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		Fail("config path can't be empty")
+	}
+
+	var err error
+	e2eCfg, err = e2eConfig.ReadE2EConfig(configPath)
+	Expect(err).ToNot(HaveOccurred())
+
+	cfg, err := runtimeconfig.GetConfig()
+	Expect(err).ToNot(HaveOccurred())
+
+	cl, err = runtimeclient.New(cfg, runtimeclient.Options{})
+	Expect(err).ToNot(HaveOccurred())
+
+	By("Deploying rancher and cert-manager", func() {
+		By("installing cert-manager", func() {
+			if isDeploymentReady(certManagerNamespace, certManagerName) {
+				By("already installed")
+			} else {
+				Expect(kubectl.RunHelmBinaryWithCustomErr(
+					"-n",
+					certManagerNamespace,
+					"install",
+					"--set",
+					"installCRDs=true",
+					"--create-namespace",
+					certManagerNamespace,
+					e2eCfg.CertManagerChartURL,
+				)).To(Succeed())
+				Eventually(func() bool {
+					return isDeploymentReady(certManagerNamespace, certManagerName)
+				}, 5*time.Minute, 2*time.Second).Should(BeTrue())
+				Eventually(func() bool {
+					return isDeploymentReady(certManagerNamespace, certManagerCAInjectorName)
+				}, 5*time.Minute, 2*time.Second).Should(BeTrue())
+			}
+		})
+
+		By("installing rancher", func() {
+			if isDeploymentReady(cattleSystemNamespace, rancherName) {
+				By("already installed")
+			} else {
+				Expect(kubectl.RunHelmBinaryWithCustomErr(
+					"-n",
+					cattleSystemNamespace,
+					"install",
+					"--set",
+					"bootstrapPassword=admin",
+					"--set",
+					"replicas=1",
+					"--set", fmt.Sprintf("hostname=%s.%s", e2eCfg.ExternalIP, e2eCfg.MagicDNS),
+					"--create-namespace",
+					rancherName,
+					fmt.Sprintf(e2eCfg.RancherChartURL),
+				)).To(Succeed())
+				Eventually(func() bool {
+					return isDeploymentReady(cattleSystemNamespace, rancherName)
+				}, 5*time.Minute, 2*time.Second).Should(BeTrue())
+			}
+		})
+	})
+
+	By("Deploying eks operator CRD chart", func() {
+		if isDeploymentReady(cattleSystemNamespace, operatorName) {
+			By("already installed")
+		} else {
+			Expect(kubectl.RunHelmBinaryWithCustomErr(
+				"-n",
+				crdChartName,
+				"install",
+				"--create-namespace",
+				"--set", "debug=true",
+				operatorName,
+				e2eCfg.CRDChart,
+			)).To(Succeed())
+
+			By("Waiting for CRDs to be created")
+			Eventually(func() bool {
+				for _, crdName := range crdNames {
+					crd := &apiextensionsv1.CustomResourceDefinition{}
+					if err := cl.Get(ctx,
+						runtimeclient.ObjectKey{
+							Name: crdName,
+						},
+						crd,
+					); err != nil {
+						return false
+					}
+				}
+				return true
+			}, 5*time.Minute, 2*time.Second).Should(BeTrue())
+		}
+	})
+
+	By("Deploying eks operator chart", func() {
+		if isDeploymentReady(cattleSystemNamespace, operatorName) {
+			By("already installed")
+		} else {
+			Expect(kubectl.RunHelmBinaryWithCustomErr(
+				"-n",
+				cattleSystemNamespace,
+				"install",
+				"--create-namespace",
+				"--set", "debug=true",
+				operatorName,
+				e2eCfg.OperatorChart,
+			)).To(Succeed())
+
+			By("Waiting for eks operator deployment to be available")
+			Eventually(func() bool {
+				return isDeploymentReady(cattleSystemNamespace, operatorName)
+			}, 5*time.Minute, 2*time.Second).Should(BeTrue())
+		}
+		// As we are not bootstrapping rancher in the tests (going to the first login page, setting new password and rancher-url)
+		// We need to manually set this value, which is the same value you would get from doing the bootstrap
+		setting := &managementv3.Setting{}
+		Expect(cl.Get(ctx,
+			runtimeclient.ObjectKey{
+				Name: "server-url",
+			},
+			setting,
+		)).To(Succeed())
+
+		setting.Source = "env"
+		setting.Value = fmt.Sprintf("https://%s.%s", e2eCfg.ExternalIP, e2eCfg.MagicDNS)
+
+		Expect(cl.Update(ctx, setting)).To(Succeed())
+
+	})
+
+	By("Creating aws credentials secret", func() {
+		secret := &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      awsCredentialsSecretName,
+				Namespace: "default",
+			},
+			Data: map[string][]byte{
+				"amazonec2credentialConfig-accessKey": []byte(e2eCfg.AWSAccessKey),
+				"amazonec2credentialConfig-secretKey": []byte(e2eCfg.AWSSecretAccessKey),
+			},
+		}
+
+		err := cl.Create(ctx, secret)
+		if err != nil {
+			fmt.Println(err)
+			Expect(apierrors.IsAlreadyExists(err)).To(BeTrue())
+		}
+	})
+
+	// By("Reading cluster templates", func() { TODO: uncomment with first tests
+	// 	assets, err := templates.ReadDir("templates")
+	// 	Expect(err).ToNot(HaveOccurred())
+
+	// 	for _, asset := range assets {
+	// 		b, err := templates.ReadFile(path.Join("templates", asset.Name()))
+	// 		Expect(err).ToNot(HaveOccurred())
+
+	// 		eksCluster := &eksv1.EKSClusterConfig{}
+	// 		Expect(yaml.Unmarshal(b, eksCluster)).To(Succeed())
+
+	// 		name := strings.TrimSuffix(asset.Name(), ".yaml")
+	// 		generatedName := names.SimpleNameGenerator.GenerateName(name + "-")
+	// 		eksCluster.Name = generatedName
+	// 		eksCluster.Spec.DisplayName = generatedName
+	// 		eksCluster.Spec.Region = e2eCfg.AWSRegion
+
+	// 		clusterTemplates[name] = eksCluster
+	// 	}
+	// })
+})
+
+var _ = AfterSuite(func() {
+	By("Creating artifact directory")
+
+	if _, err := os.Stat(e2eCfg.ArtifactsDir); os.IsNotExist(err) {
+		Expect(os.Mkdir(e2eCfg.ArtifactsDir, os.ModePerm)).To(Succeed())
+	}
+
+	By("Getting eks operator logs")
+
+	podList := &corev1.PodList{}
+	Expect(cl.List(ctx, podList, runtimeclient.MatchingLabels{
+		"ke.cattle.io/operator": "eks",
+	}, runtimeclient.InNamespace(cattleSystemNamespace),
+	)).To(Succeed())
+
+	for _, pod := range podList.Items {
+		for _, container := range pod.Spec.Containers {
+			output, err := kubectl.Run("logs", pod.Name, "-c", container.Name, "-n", pod.Namespace)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(os.WriteFile(filepath.Join(e2eCfg.ArtifactsDir, pod.Name+"-"+container.Name+".log"), redactSensitiveData([]byte(output)), 0644)).To(Succeed())
+		}
+	}
+
+	By("Getting eks Clusters")
+
+	eksClusterList := &eksv1.EKSClusterConfigList{}
+	Expect(cl.List(ctx, eksClusterList, &runtimeclient.ListOptions{})).To(Succeed())
+
+	for _, eksCluster := range eksClusterList.Items {
+		output, err := yaml.Marshal(eksCluster)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(os.WriteFile(filepath.Join(e2eCfg.ArtifactsDir, "eks-cluster-config-"+eksCluster.Name+".yaml"), redactSensitiveData([]byte(output)), 0644)).To(Succeed())
+	}
+
+	By("Getting Rancher Clusters")
+
+	rancherClusterList := &managementv3.ClusterList{}
+	Expect(cl.List(ctx, rancherClusterList, &runtimeclient.ListOptions{})).To(Succeed())
+
+	for _, rancherCluster := range rancherClusterList.Items {
+		output, err := yaml.Marshal(rancherCluster)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(os.WriteFile(filepath.Join(e2eCfg.ArtifactsDir, "rancher-cluster-"+rancherCluster.Name+".yaml"), redactSensitiveData([]byte(output)), 0644)).To(Succeed())
+	}
+
+	By("Cleaning up Rancher Clusters")
+
+	for _, rancherCluster := range rancherClusterList.Items {
+		Expect(cl.Delete(ctx, &rancherCluster)).To(Succeed())
+		Eventually(func() error {
+			return cl.Get(ctx, runtimeclient.ObjectKey{
+				Name:      rancherCluster.Name,
+				Namespace: rancherCluster.Namespace,
+			}, &eksv1.EKSClusterConfig{})
+		}, waitLong, pollInterval).ShouldNot(Succeed())
+	}
+})
+
+func isDeploymentReady(namespace, name string) bool {
+	deployment := &appsv1.Deployment{}
+	if err := cl.Get(ctx,
+		runtimeclient.ObjectKey{
+			Namespace: namespace,
+			Name:      name,
+		},
+		deployment,
+	); err != nil {
+		return false
+	}
+
+	if deployment.Status.AvailableReplicas == *deployment.Spec.Replicas {
+		return true
+	}
+
+	return false
+}
+
+func redactSensitiveData(input []byte) []byte {
+	output := bytes.Replace(input, []byte(e2eCfg.AWSAccessKey), []byte("***"), -1)
+	output = bytes.Replace(output, []byte(e2eCfg.AWSSecretAccessKey), []byte("***"), -1)
+	output = bytes.Replace(output, []byte(e2eCfg.AWSRegion), []byte("***"), -1)
+	return output
+}
