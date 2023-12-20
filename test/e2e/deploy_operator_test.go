06079c64d681bf13bbc9452c9ad10768289d2ca9
Furkat Gofurov <furkat.gofurov@suse.com>
2023-12-20 17:47:17 +0200

    Add new Makefile target to deploy operator using kind
    Signed-off-by: Furkat Gofurov <furkat.gofurov@suse.com>

---
+++ test/e2e/deploy_operator_test.go
@@ -0,0 +1,9 @@
+package e2e
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+)
+
+var _ = Describe("Do nothing, used to deploy rancher and operator", Label("do-nothing"), func() {
+	It("Does nothing", func() {})
+})
