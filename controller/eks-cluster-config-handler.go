6761e3dd4d899453783a7e04fda4fa6cc24e3a21
Michal Jura <mjura@suse.com>
2024-08-22 07:14:26 +0200

    Fixing non-constant format string in call
    (cherry picked from commit 0ec28e48ed8823444c076da896f550c4665e32c8)

--- controller/eks-cluster-config-handler.go
+++ controller/eks-cluster-config-handler.go
@@ -414,7 +414,7 @@ func validateUpdate(config *eksv1.EKSClusterConfig) error {
 			"must be equal to or one minor version lower than the cluster kubernetes version", aws.StringValue(config.Spec.KubernetesVersion), aws.StringValue(ng.Version)))
 	}
 	if len(errs) != 0 {
-		return fmt.Errorf(strings.Join(errs, ";"))
+		return fmt.Errorf("%s", strings.Join(errs, ";"))
 	}
 	return nil
 }
