bbe9c687ce04817f9e7db6d5f3dd56b035553198
yiannistri <8741709+yiannistri@users.noreply.github.com>
2024-09-17 19:59:41 +0100

    fix: Simplified error messages
    (cherry picked from commit c48a5515f1905669427cc73a46df97ba50b5eade)

--- controller/eks-cluster-config-handler_test.go
+++ controller/eks-cluster-config-handler_test.go
@@ -209,19 +209,27 @@ var _ = Describe("updateCluster", func() {
 		Expect(err).To(MatchError("node group name [ng1] is not unique within the cluster [test (id: test)] to avoid duplication"))
 	})
 
-	It("should not allow node group versions outside version skew", func() {
-		tooOldVersion := "1.21"
-		tooOldNodeGroup := []eksv1.NodeGroup{
-			{
-				NodegroupName: aws.String("ng_1.21"),
-				Version:       &tooOldVersion,
-			},
-		}
-
+	It("should not allow node group versions outside version skew for Kubernetes < 1.25", func() {
 		eksConfig.Status.Phase = "active"
-		eksConfig.Spec.NodeGroups = append(eksConfig.Spec.NodeGroups, tooOldNodeGroup...)
+		eksConfig.Spec.KubernetesVersion = aws.String("1.24")
+		eksConfig.Spec.NodeGroups = append(eksConfig.Spec.NodeGroups, eksv1.NodeGroup{
+			NodegroupName: aws.String("ng2"),
+			Version:       aws.String("1.21"),
+		})
 		_, err := handler.OnEksConfigChanged("", eksConfig)
-		Expect(err).To(MatchError("versions for cluster [1.25] and nodegroup [1.21] not compatible: all nodegroup kubernetes versions " +
-			"must within version skew policy (K8s < 1.25: N-2, K8s >= 1.25: N-3)"))
+		Expect(err).To(MatchError("versions for cluster [1.24] and node group [1.21] are not compatible: for clusters running Kubernetes < 1.25 the " +
+			"node group version may only be up to two minor versions older than the cluster version"))
+	})
+
+	It("should not allow node group versions outside version skew for Kubernetes >= 1.25", func() {
+		eksConfig.Status.Phase = "active"
+		eksConfig.Spec.KubernetesVersion = aws.String("1.25")
+		eksConfig.Spec.NodeGroups = append(eksConfig.Spec.NodeGroups, eksv1.NodeGroup{
+			NodegroupName: aws.String("ng2"),
+			Version:       aws.String("1.21"),
+		})
+		_, err := handler.OnEksConfigChanged("", eksConfig)
+		Expect(err).To(MatchError("versions for cluster [1.25] and node group [1.21] are not compatible: for clusters running Kubernetes >= 1.25 the " +
+			"node group version may only be up to three minor versions older than the cluster version"))
 	})
 })
